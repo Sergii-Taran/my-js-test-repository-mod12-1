a17d116f8de316bfa335e0fba4349ae3f80c6397
Sergii-Taran <serg.taran1970@gmail.com>
2025-10-23 19:42:21 +0000

    Deploying to gh-pages from @ Sergii-Taran/my-js-test-repository-mod12-1@677b33b95979a4a77bf1b3cb07f7df3f80d011d1 🚀

---
+++ 1-daily-tasks.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{a}from"./assets/vendor-CWxt7QI6.js";const t="http://localhost:3000/todos";a(t,{params:{api_key:"lalala",limit:10}}).then(o=>console.log(o.data)).catch(o=>console.log(o));
+//# sourceMappingURL=1-daily-tasks.js.map
