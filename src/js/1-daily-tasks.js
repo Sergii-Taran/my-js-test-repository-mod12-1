4ed8de663537657d0a27d68fb4682934d08e5b7d
Serhii Taran <serg.taran1970@gmail.com>
2025-10-24 22:45:29 +0300

    add-full-daily-tasks

--- src/js/1-daily-tasks.js
+++ src/js/1-daily-tasks.js
@@ -1,22 +1,101 @@
 import axios from 'axios';
 
-import '../css/styles.css';
-
 const BASE_URL = 'http://localhost:3000/todos';
 
-axios(BASE_URL, {
-  params: {
-    api_key: 'lalala',
-    limit: 10,
-  },
-})
-  .then(response => console.log(response.data))
-  .catch(error => console.log(error));
+const form = document.querySelector('.todo__form');
+const container = document.querySelector('.list');
 
-// axios
-//   .post(BASE_URL, {
-//     title: 'купити хліб',
-//     completed: false,
-//   })
-//   .then(response => console.log(response))
-//   .catch(error => console.log(error.message));
+form.addEventListener('submit', handleSubmit);
+container.addEventListener('click', handleUpdate);
+container.addEventListener('click', handleDelete);
+
+async function serviceTodos(url = BASE_URL, options = {}) {
+  const result = await axios(url, options);
+  return result.data;
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ id, title, completed }) => `
+  <li class="list__item" data-id="${id}">
+  <input type="checkbox" class="list__checkbox" ${completed ? 'checked' : ''}>
+  <h2 class="list__title">${title}</h2>
+  <button class="list__btn">X</button>
+  </li>
+  `
+    )
+    .join('');
+}
+
+serviceTodos(BASE_URL)
+  .then(res => container.insertAdjacentHTML('beforeend', createMarkup(res)))
+  .catch(error => console.log(error.message));
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const { todo } = event.currentTarget.elements;
+
+  if (!todo.value.trim) {
+    return;
+  }
+  try {
+    const data = await serviceTodos(BASE_URL, {
+      method: 'POST',
+      data: {
+        title: todo.value,
+        completed: false,
+      },
+    });
+
+    container.insertAdjacentHTML('beforeend', createMarkup([data]));
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    event.target.reset();
+  }
+}
+
+async function handleUpdate(event) {
+  if (!event.target.classList.contains('list__checkbox')) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const parent = event.target.closest('.list__item');
+  const id = parent.dataset.id;
+
+  try {
+    const data = await serviceTodos(`${BASE_URL}/${id}`, {
+      method: 'PATCH',
+      data: {
+        completed: event.target.checked,
+      },
+    });
+
+    event.target.checked = data.completed;
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+async function handleDelete(event) {
+  if (!event.target.classList.contains('list__btn')) {
+    return;
+  }
+
+  const parent = event.target.closest('.list__item');
+  const id = parent.dataset.id;
+
+  try {
+    await serviceTodos(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+    });
+
+    parent.remove();
+  } catch (error) {
+    console.log(error);
+  }
+}
