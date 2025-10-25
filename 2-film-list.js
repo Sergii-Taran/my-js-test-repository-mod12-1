f5ac7a6857f49674d62076383c2f8298cbc22203
Sergii-Taran <serg.taran1970@gmail.com>
2025-10-25 20:05:16 +0000

    Deploying to gh-pages from @ Sergii-Taran/my-js-test-repository-mod12-1@67d0179e1b32f89087c54c6306f54813210de362 🚀

--- 2-film-list.js
+++ 2-film-list.js
@@ -1,11 +1,11 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{a as l}from"./assets/vendor-CWxt7QI6.js";const m="https://api.themoviedb.org/3",p="/trending/movie/week",g="345007f9ab440e5b86cef51be6397df1",c=document.querySelector(".js-movie-list "),t=document.querySelector(".js-load-more");t.addEventListener("click",u);let r=1;async function i(e=1){const{data:o}=await l(`${m}${p}`,{params:{api_key:g,page:e}});return o}i(r).then(e=>{c.insertAdjacentHTML("beforeend",n(e.results)),e.page<e.total_pages&&t.classList.replace("load-more-hidden","load-more")}).catch(e=>alert(e.message));function n(e){return e.map(({poster_path:o,release_date:a,original_title:s,vote_average:d})=>`
-    <li class="movie-card">
-       <img src="https://image.tmdb.org/t/p/w300${o}" alt="${s}">
-       <div class="movie-info">
-          <h2>${s}</h2>
-          <p>Release Date: ${a}</p>
-          <p>Vote Average: ${d}</p>    
-       </div>
-       </li>
-    `).join("")}async function u(){r+=1,t.disabled=!0;try{const e=await i(r);c.insertAdjacentHTML("beforeend",n(e.results)),e.page>=e.total_pages&&t.classList.replace("load-more","load-more-hidden");const a=document.querySelector(".movie-card").getBoundingClientRect().height;window.scrollBy({left:0,top:a,behavior:"smooth"})}catch(e){alert(e.message)}finally{t.disabled=!1}}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{a as p}from"./assets/vendor-CWxt7QI6.js";const g="https://api.themoviedb.org/3",l="/trending/movie/week",m="345007f9ab440e5b86cef51be6397df1",s=document.querySelector(".js-movie-list"),d=document.querySelector(".js-guard"),u={root:null,rootMargin:"300px",threshold:0},f=new IntersectionObserver(v,u);let a=1;async function n(e=1){const{data:o}=await p(`${g}${l}`,{params:{api_key:m,page:e}});return o}function i(e){return e.map(({poster_path:o,release_date:r,original_title:t,vote_average:c})=>`
+        <li class="movie-card">
+            <img src="https://image.tmdb.org/t/p/w300${o}" alt="${t}">
+            <div class="movie-info">
+                <h2>${t}</h2>
+                <p>Release Date: ${r}</p>
+                <p>Vote Average: ${c}</p>
+            </div>
+        </li>
+    `).join("")}n().then(e=>{s.insertAdjacentHTML("beforeend",i(e.results)),e.page<e.total_pages&&f.observe(d)}).catch(e=>alert(e.message));function v(e,o){e.forEach(async r=>{if(r.isIntersecting){a+=1;try{const t=await n(a);s.insertAdjacentHTML("beforeend",i(t.results)),t.page>=t.total_pages&&o.unobserve(r.target)}catch(t){alert(t.message)}}})}
 //# sourceMappingURL=2-film-list.js.map
